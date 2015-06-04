5cef5f9652a7da55a08adf69ccffd5c4c9b854fe
matteopasina <matteopasina@gmail.com>
2015-06-04 14:28:16 +0200

    www senza script sotto

--- www/js/app.js
+++ www/js/app.js
@@ -3,38 +3,38 @@ var app=    angular.module('RoutingApp', ['ngRoute','ui.bootstrap','smoothScroll
 app.config(function ($routeProvider) {
     $routeProvider
         .when('/', {
-            templateUrl: '/views/Home.html'
+            templateUrl: '/www/views/Home.html'
         })
         .when('/CourseHome/', {
             controller: 'CourseHomeController',
-            templateUrl: '/views/CourseHome.html'
+            templateUrl: '/www/views/CourseHome.html'
         })
         .when('/course/:id', {
             controller: 'CoursesJoinInstructor',
-            templateUrl: '/views/Course.html'
+            templateUrl: '/www/views/Course.html'
         })
         .when('/courseByLevel/', {
             controller: 'CourseLevelController',
-            templateUrl: '/views/CourseLevel.html'
+            templateUrl: '/www/views/CourseLevel.html'
         })
         .when('/coursesByCategory/', {
             controller: 'CourseByCourseCategoryController',
-            templateUrl: '/views/CourseLevel.html'
+            templateUrl: '/www/views/CourseLevel.html'
         })
         .when('/instructor/:id', {
             controller: 'InstructorController',
-            templateUrl: '/views/Instructor.html'
+            templateUrl: '/www/views/Instructor.html'
         })
         .when('/Location', {
-            templateUrl: '/views/Location.html'
+            templateUrl: '/www/views/Location.html'
         })
         .when('/Categories/', {
             controller: 'CategoriesController',
-            templateUrl: '/views/Categories.html'
+            templateUrl: '/www/views/Categories.html'
         })
         .when('/form/:id', {
             controller: 'CourseController',
-            templateUrl: '/views/Form.html'
+            templateUrl: '/www/views/Form.html'
         })
         .otherwise({
             redirectTo: '/'
