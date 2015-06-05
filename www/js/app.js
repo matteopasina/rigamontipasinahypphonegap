09815511cfb033af714020eb6107ca7bc1223a29
matteopasina <matteopasina@gmail.com>
2015-06-05 16:34:58 +0200

    odino

--- www/js/app.js
+++ www/js/app.js
@@ -1,40 +1,40 @@
-var app=angular.module('RoutingApp', ['ngRoute','ui.bootstrap','smoothScroll','ngCordova']);
+var app=    angular.module('RoutingApp', ['ngRoute','ui.bootstrap','smoothScroll']);
 
 app.config(function ($routeProvider) {
     $routeProvider
         .when('/', {
-            templateUrl: '/www/views/Home.html'
+            templateUrl: 'views/Home.html'
         })
         .when('/CourseHome/', {
             controller: 'CourseHomeController',
-            templateUrl: '/www/views/CourseHome.html'
+            templateUrl: 'views/CourseHome.html'
         })
         .when('/course/:id', {
             controller: 'CoursesJoinInstructor',
-            templateUrl: '/www/views/Course.html'
+            templateUrl: 'views/Course.html'
         })
         .when('/courseByLevel/', {
             controller: 'CourseLevelController',
-            templateUrl: '/www/views/CourseLevel.html'
+            templateUrl: 'views/CourseLevel.html'
         })
         .when('/coursesByCategory/', {
             controller: 'CourseByCourseCategoryController',
-            templateUrl: '/www/views/CourseLevel.html'
+            templateUrl: 'views/CourseLevel.html'
         })
         .when('/instructor/:id', {
             controller: 'InstructorController',
-            templateUrl: '/www/views/Instructor.html'
+            templateUrl: 'views/Instructor.html'
         })
         .when('/Location', {
-            templateUrl: '/www/views/Location.html'
+            templateUrl: 'views/Location.html'
         })
         .when('/Categories/', {
             controller: 'CategoriesController',
-            templateUrl: '/www/views/Categories.html'
+            templateUrl: 'views/Categories.html'
         })
         .when('/form/:id', {
             controller: 'CourseController',
-            templateUrl: '/www/views/Form.html'
+            templateUrl: 'views/Form.html'
         })
         .otherwise({
             redirectTo: '/'
